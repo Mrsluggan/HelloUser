12ca782b484be33b61745755598e748f8cd295a7
Mrsluggan <ericflyger@gmail.com>
2023-10-31 13:39:08 +0100

    Start av projekt -
    Pushar Springboot + thymleaf

---
+++ medlemssida/src/main/java/com/medlemssida/medlemssida/MedlemssidaApplication.java
@@ -0,0 +1,13 @@
+package com.medlemssida.medlemssida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlemssidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlemssidaApplication.class, args);
+	}
+
+}
